d5806b94cbdb729542df81ffd633c4ecbec11ae7
spark <spark@dongbu.com>
2016-02-15 00:00:40 +0900

    Add Message - Alert 3

--- Hello World/src/TestAlert2.java
+++ Hello World/src/TestAlert2.java
@@ -4,6 +4,7 @@ public class TestAlert2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Alert 2!");
+		System.out.println("Alert 3!");
 	}
 
 }
